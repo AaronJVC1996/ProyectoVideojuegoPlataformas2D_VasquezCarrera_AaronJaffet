4bbd40ada0d39164c1999f96d1febee5a2a1cf65
avasquecar1 <avasquecar1@educacion.navarra.es>
2024-11-04 15:43:45 +0100

    04/11/2024 parte2
    fallaba el script de obstaculos, no se logro guardar lo cambiado, y lo he vuelto a hacer y vuelto a meter dentro de los obstaculos

--- Assets/Scripts/ObstaculosDamage.cs
+++ Assets/Scripts/ObstaculosDamage.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ObstaculosDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int damage = 1; // Daño del ataque
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player")) // Verifica si el collider es el jugador
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.TakeDamage(damage); // Llama al método para restar vida al jugador
+                // Aquí puedes agregar una animación si lo deseas
+            }
+        }
     }
 }
